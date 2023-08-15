44df5fbe24e57daa0a772d1e063c6d9131b930da
OngkyRinaldo <edoorinaldoo@gmail.com>
2023-08-15 16:15:01 +0700

    update main.js

--- src/main.jsx
+++ src/main.jsx
@@ -3,16 +3,13 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
-import { HomeProvider } from './context/home.context.jsx';
 import { TitleProvider } from './context/dynamicTitle.context.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <BrowserRouter>
             <TitleProvider>
-                <HomeProvider>
-                    <App />
-                </HomeProvider>
+                <App />
             </TitleProvider>
         </BrowserRouter>
     </React.StrictMode>
